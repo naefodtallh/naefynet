20ea00dcb9d0ac7cb4e1664e3bdb8b317a5322a0
naef odtallh <naef2595@gmail.com>
2021-10-07 15:58:43 +0300

    the code

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import pandas as pd
+from lxml import etree
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/ynet')
+def ynet():
+    data = "C:/Path/test.xml"
+    tree = etree.parse(data)
+    lstKey = []
+     lstValue = []
+    for p in tree.iter() :
+    lstKey.append(tree.getpath(p).replace("/",".")[1:])
+    lstValue.append(p.text)
+    df = pd.DataFrame({'key' : lstKey, 'value' : lstValue})
+    df.sort_values('key')
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
+
